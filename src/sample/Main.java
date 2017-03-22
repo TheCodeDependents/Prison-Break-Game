bd89b4e2d063087342db056374216e56db0ccbad
TheCodeDependents <erik.s.nestor@gmail.com>
2017-03-21 21:41:51 -0400

    Board is visible.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,55 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+
+// -----------------------------------
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        ScrollPane pane = new ScrollPane();
+        Canvas canvas = new Canvas(3000, 3000);
+
+        pane.setContent( canvas );
+        pane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        pane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        drawBoard(canvas);
+
+        primaryStage.setTitle("Prison Break!");
+        Scene scene = new Scene(pane, 3000,3000);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        //root.getChildren().add( canvas );
+        //ScrollPane sp = createScrollPane(root);
+    }
+
+    private void drawBoard(Canvas canvas) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.ORANGE);
+        gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
+        gc.setFill(Color.YELLOW);
+        gc.setStroke(Color.GREY);
+        gc.setLineWidth(1);
+        gc.strokeRect(100,100,2800,2800);
+        gc.fillRect(100, 100, 2800, 2800);
+        for (int i = 200; i < 2900; i += 100) {
+            gc.strokeLine(i, 100, i, 2900);
+            gc.strokeLine(100, i, 2900, i);
+        }
+    }
 }
+
